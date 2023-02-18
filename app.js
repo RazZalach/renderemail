bc24a91d52961fe9b85d11219c57dd6416c74bd9
Raz <110323932+Idus-Raz@users.noreply.github.com>
2023-02-18 13:31:15 +0200

    fiile upload

--- app.js
+++ app.js
@@ -3,21 +3,39 @@ const express=require('express');
 const app=express();
 const morgan=require('morgan');
 const hbs=require('hbs');
+const multer=require('multer');
 const port = process.env.PORT || 3377;
 
 app.set((__dirname));
 app.set('view engine','hbs');
-
+app.use('/uploads', express.static('uploads'));
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-
-app.post('/raz/:email',(req,res)=>{
-    console.log(req.params);
-    res.status(200).json({msg:"fdssd"});
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        if(file.fieldname == 'profilePic')
+            cb(null,'./uploads/pics/');
+        else
+            cb(null,'./uploads/files/');
+    },
+    filename:(req,file,cb)=>{
+        let filename= Math.floor( Math.random() * 100000);
+        let fileExtension = file.originalname.split('.').pop();// של הסיומת שליפת הקובץ
+        cb(null,filename+"."+fileExtension);
+    }
 });
+const uploadFiles=multer({
+    storage:storage
+});
+   
+app.post("/register",uploadFiles.single("profilePic"),(req,res)=>{
+    const fileUrl = `${req.protocol}://${req.get('host')}/${req.file.path}`;
+    res.status(200).json({fileUrl});
+});    
+
 
 app.post('/contact',(req,res)=>{
   
@@ -48,4 +66,7 @@ app.get('/emailsend',(req,res)=>{
 });
 app.get('/',(req,res)=>{
     res.render('vocal');
+});
+app.get('/uploads',(req,res)=>{
+    res.render('uploadfile');
 });
